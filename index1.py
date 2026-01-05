da9a68bc1932a91c7f6f13abb7387c6c1172afbf
Nahdiya-younus <nahdiyayounus@gmail.com>
2026-01-05 23:33:43 +0530

    new change

--- index1.py
+++ index1.py
@@ -2,3 +2,8 @@ import pygame
 x = pygame.init()
 gameWindow = pygame.display.set_mode((1200, 500))
 pygame.display.set_caption("Flappy Bird")
+
+# initializing variables
+game_over = False 
+exit_game = False
+
