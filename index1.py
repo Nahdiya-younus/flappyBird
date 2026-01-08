b5d6c8f1adc6ea11065a17f911ed4e3e97f3d065
Nahdiya-younus <nahdiyayounus@gmail.com>
2026-01-08 22:43:36 +0530

    background completed

--- index1.py
+++ index1.py
@@ -3,22 +3,34 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 gameWindow = pygame.display.set_mode((604, 500))
 pygame.display.set_caption("Flappy Bird")
 
+#define game variables
+ground_scroll = 0 
+scroll_speed = 4
+
 #load images
 bg = pygame.image.load("background.jpg")
-ground_img = pygame.image.load("ground1.jpg")
+ground_img = pygame.image.load("ground2.jpg")
 
 run = True 
 #creating a game loop
 while run:
 
+    clock.tick(fps)
+
     #draw background
     gameWindow.blit(bg, (0,0))
 
     #draw and scroll the ground 
-    gameWindow.blit(ground_img, (0,350))
+    gameWindow.blit(ground_img, (ground_scroll,350))
+    ground_scroll -= scroll_speed 
+    if abs(ground_scroll)> 55:
+        ground_scroll = 0
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
